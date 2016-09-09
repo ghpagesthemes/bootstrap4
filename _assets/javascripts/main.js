4f8cec10c3bcecbcdaae37af6243647439bc4084
Michael Shick <m@shick.us>
2016-09-08 18:02:45 -0700

    remove redunant js

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,7 +1,8 @@
+import 'core-js/es6';
+import svg4everybody from 'svg4everybody';
+import ready from './ready';
 import './globals/jquery.js';
 import './globals/tether.js';
 import 'bootstrap/dist/js/bootstrap';
-import svg4everybody from 'svg4everybody';
-import ready from './ready';
 
 ready(() => svg4everybody());
